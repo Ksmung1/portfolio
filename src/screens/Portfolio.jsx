8d164348ec37e055e98f47b0d8bb90efa563c618
crepybozz <mungthomte@gmail.com>
2025-03-23 23:04:47 +0530

    edited bar graph

--- src/screens/Portfolio.jsx
+++ src/screens/Portfolio.jsx
@@ -35,7 +35,7 @@ const Portfolio = () => {
     {
       label: "CRYPTO",
       coins: [
-        { date: 'Date', invested: 'Invested ($)', ppt: 'Price Per Token ($)', name: "Coins", amount: "Quantity", isHeading: true, currentPrice: "Current Price" },
+        { date: 'Date', invested: 'Invested ($)', ppt: 'Buying Price ($)', name: "Coins", amount: "Quantity", isHeading: true, currentPrice: "Current Price" },
         { date: '14/03/25', invested: '500', ppt: '0.0023', name: "KLV", amount: "215796", currentPrice: prices.KLV ? `$${prices.KLV}` : "Loading..." },  // ✅ Dynamic KLV price
         { date: '13/03/25', invested: '500', ppt: '2.224', name: "XRP", amount: "224.82", currentPrice: prices.XRP ? `$${prices.XRP}` : "Loading..." },  // ✅ Dynamic XRP price
       ],
